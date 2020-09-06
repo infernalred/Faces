44c15be79de92ab90cda321a4d225c81a1582559
Aleksandr Larochkin <infernalred13@gmail.com>
2020-09-06 23:14:41 +0300

    section 5

---
+++ Faces.SharedLib/Messaging.InterfacesConstants/Constants/RabbitMqMassTransitConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.InterfacesConstants.Constants
+{
+    public class RabbitMqMassTransitConstants
+    {
+        public const string RabbitMqUri = "rabbitmq://rabbitmq:5672";
+        public const string UserName = "quest";
+        public const string Password = "quest";
+        public const string RegisterOrderCommandQueue = "register.order.command";
+    }
+}
